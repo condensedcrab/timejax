72e382f2aa7872adfbad5af5210989a758945cd0
David <david.barbalas@gmail.com>
2025-02-03 22:05:38 -0800

    updating column names for both datasets

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# %%
 # using CO2 and CH4 datasets from NOAA
 # https://gml.noaa.gov/ccgg/trends/
 
@@ -7,10 +8,26 @@ import os
 import matplotlib.pyplot as plt
 import scipy
 import numpy as np
+
 import time
 import pandas as pd
 import jax.numpy as jnp
 
 # %% load data
 
-df = pd.read_csv("data/co2_daily_mlo.csv")
+co2 = pd.read_csv(
+    "data/co2_daily_mlo.csv",
+    skiprows=32,
+    names=["year", "month", "day", "timestamp", "ppm"],
+)
+ch4 = pd.read_csv(
+    "data/ch4_mm_gl.csv",
+    skiprows=46,
+    names=["year", "month", "timestamp", "ppb", "std", "ppb_2", "std_2"],
+)
+
+plt.figure()
+plt.subplot(121)
+plt.plot(co2)
+
+# %%
