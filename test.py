65f0b709a5df956e97e6136a153a80dcf799bef4
David <david.barbalas@gmail.com>
2025-02-03 22:26:09 -0800

    trying to play around with the FT

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ import time
 import pandas as pd
 import jax.numpy as jnp
 
-# %% load data
+# %% load data and plot data
 
 co2 = pd.read_csv(
     "data/co2_daily_mlo.csv",
@@ -29,13 +29,27 @@ ch4["ppm"] = ch4["ppm"] / 1000
 
 plt.figure(figsize=(12, 4))
 plt.subplot(121)
-plt.plot(co2["timestamp"], co2["ppm"])
+plt.plot(co2["timestamp"], co2["ppm"], color="maroon")
 plt.xlabel("Time (yr)"), plt.ylabel("CO$_2$ Concentration (ppm)")
 plt.grid("both")
 
 plt.subplot(122)
-plt.plot(ch4["timestamp"], ch4["ppm"])
+plt.plot(ch4["timestamp"], ch4["ppm"], color="dodgerblue")
 plt.xlabel("Time (yr)"), plt.ylabel("Avg. CH$_4$ Concentration (ppb)")
 plt.grid("both")
 
-# %%
+# %% CO2 data - 0th order FFT analysis
+X = co2["timestamp"].to_numpy()
+timestep = jnp.diff(X)[0]
+N = len(X)
+
+freq = np.fft.fftfreq(N, timestep)[: N // 2]
+F = jnp.fft.fft(X)
+
+
+plt.figure()
+
+plt.plot(freq, np.abs(F[: N // 2]))
+# plt.yscale("log")
+# plt.xlim([120,150])
+# plt.ylim([0,10])
